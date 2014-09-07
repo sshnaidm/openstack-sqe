53ac556904d99e2ba6db37ed7ff81cad9e1eaffb
Sergey Shnaidman <sshnaidm@cisco.com>
2014-09-07 14:56:23 +0400

    tools/github_poller.py

--- tools/github_poller.py
+++ tools/github_poller.py
@@ -1,38 +1,24 @@
 #!/usr/bin/env python
-__author__ = 'Sergey Shnaidman <sshnaidm@cisco.com>'
-
-import os
+import argparse
 import time
 import ConfigParser
-#import pygithub3
 from pygithub3 import Github
 import requests
 import json
 import urllib3
-import sys
 from itertools import chain
 from datetime import datetime
 import logging
-#from gevent import monkey
-#monkey.patch_all()
 
-#pygithub3.services.base.Service(login="terrrakot", password="hot78dog")
+__author__ = 'Sergey Shnaidman <sshnaidm@cisco.com>'
 
-LOG_FILENAME = 'github_poller.log'
-CONFIG_FILE="./github_poller.cfg"
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s %(levelname)-8s %(message)s',
-                    datefmt='%a, %d %b %Y %H:%M:%S',
-                    filename=LOG_FILENAME,
-                    filemode='w')
-log = logging.getLogger('log')
 
-def parse_config():
+def parse_config(config_file):
     log.debug("Parsing config")
     parser = ConfigParser.SafeConfigParser()
     parser.optionxform = str
-    parser.read(CONFIG_FILE)
-    config = {}
+    parser.read(config_file)
+    config = dict()
     config['repos'] = dict(parser.items("repos"))
     config['repo_poll'] = int(parser.get("timeouts", "repo_poll"))
     config['jenkins'] = dict(parser.items("jenkins"))
@@ -40,6 +26,7 @@ def parse_config():
     config['credentials'] = dict(parser.items("github"))
     log.debug("Configuration: %s" % str(config))
     return config
+
 
 def get_last_commit(config, repo, branch):
     log.debug("Getting last commit from repo: %s branch: %s" % (repo, branch))
@@ -57,6 +44,7 @@ def get_last_commit(config, repo, branch):
         log.debug("%s:%s FAIL to retrieve last commit:\n%s" % (repo, branch, str(e)))
         return ""
 
+
 def get_start_values(config):
     start = {}
     for repo, branch in config['repos'].items():
@@ -64,28 +52,31 @@ def get_start_values(config):
         start[repo] = get_last_commit(config, repo, branch)
     return start
 
-def check_n_trigger(config, start):
+
+def check_n_trigger(config, start, conf_file):
     for repo, branch in config['repos'].items():
         latest = get_last_commit(config, repo, branch)
         log.debug("%s:%s Check-n-trigger: start_commit=%s and last_commit=%s" % (
             repo, branch, start[repo], latest))
         if start[repo].sha != latest.sha:
-            trigger_jenkins(config, repo, branch, start[repo], latest)
+            trigger_jenkins(config, repo, branch, start[repo], latest, conf_file)
             start[repo] = latest
         #time.sleep(config['commit_poll'])
 
-def trigger_jenkins(conf, repo, branch, start, end):
-    log.debug("%s:%s Triggering Jenkins job!" % (repo, branch))
+
+def trigger_jenkins(conf, repo, branch, start, end, conf_file):
+    log.info("%s:%s Triggering Jenkins job!" % (repo, branch))
     diff = pretty_print_diff(calculate_diff(conf, repo, branch, start, end))
-    log.debug("%s:%s Difference is:\n%s"  % (repo, branch, diff))
+    log.info("%s:%s Difference is:\n%s" % (repo, branch, diff))
     jenkins_url = (conf["jenkins"]["url"] + "/job/" + conf["jenkins"]["job"] + "/build" +
                    "?token=" + conf["jenkins"]["token"])
-    log.debug("%s:%s Jenkins URL=%s"% (repo, branch, jenkins_url))
-    params = {"parameter":
-                  [{"name": "tag", "value": "network"},
-                   {"name": "changes_file", "file": "file0"}],
-              "statusCode": "303", "redirectTo": "."}
-    log.debug("%s:%s Parameters for Jenkins URL: %s"% (repo, branch, str(params)))
+    log.info("%s:%s Jenkins URL=%s" % (repo, branch, jenkins_url))
+    params = {"parameter": [
+        {"name": "tag", "value": "network"},
+        {"name": conf_file, "file": "file0"}],
+        "statusCode": "303",
+        "redirectTo": "."}
+    log.info("%s:%s Parameters for Jenkins URL: %s" % (repo, branch, str(params)))
     data, content_type = urllib3.encode_multipart_formdata([
         ("file0", ("changes_file", diff)),
         ("json", json.dumps(params)),
@@ -95,10 +86,11 @@ def trigger_jenkins(conf, repo, branch, start, end):
                          data=data,
                          headers={"content-type": content_type},
                          verify=False)
-    log.debug("%s:%s Response from Jenkins: %s\n%s" % (
+    log.info("%s:%s Response from Jenkins: %s\n%s" % (
         repo, branch, str(resp.status_code), resp.content))
     if not resp.ok:
-        log.debug("Failed to post URL: %s" % jenkins_url)
+        log.info("Failed to post URL: %s" % jenkins_url)
+
 
 def calculate_diff(config, repo, branch, start, end):
     log.debug("%s:%s Calculate diff from %s to %s" % (repo, branch, start, end))
@@ -123,6 +115,7 @@ def calculate_diff(config, repo, branch, start, end):
             diff[0].sha,
             user=user,
             repo=rep)
+
 
 def pretty_print_diff(delta):
     if delta is None:
@@ -151,13 +144,30 @@ def pretty_print_diff(delta):
     text += "</ul>"
     return text
 
+
 def main():
-    conf = parse_config()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', action='store', dest='config_file', default="changes_file",
+                        help='Configuration file path')
+    parser.add_argument('-l', action='store', dest='log_file', default="./github_poller.log",
+                        help='Log file path')
+    parser.add_argument('--version', action='version', version='%(prog)s 2.0')
+
+    opts = parser.parse_args()
+    logging.basicConfig(level=logging.DEBUG,
+                        format='%(asctime)s %(levelname)-8s %(message)s',
+                        datefmt='%a, %d %b %Y %H:%M:%S',
+                        filename=opts.log_file,
+                        filemode='w')
+    global log
+    log = logging.getLogger('log')
+
+    conf = parse_config(opts.config_file)
     start_values = get_start_values(conf)
     while True:
         log.debug("Sleeping %s seconds" % conf['repo_poll'])
         time.sleep(conf['repo_poll'])
-        check_n_trigger(conf, start_values)
+        check_n_trigger(conf, start_values, opts.config_file)
 
 if __name__ == "__main__":
     main()
